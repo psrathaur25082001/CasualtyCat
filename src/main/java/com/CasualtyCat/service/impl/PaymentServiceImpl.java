b8b55c504f22f9b574232e4555bc44ef4f8d5208
psrathaur25082001 <psrathaur25082001@gmail.com>
2024-04-13 17:58:19 +0530

    Update PaymentServiceImpl.java
    update payment service impl

--- src/main/java/com/CasualtyCat/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/CasualtyCat/service/impl/PaymentServiceImpl.java
@@ -1,4 +1,7 @@
 package com.CasualtyCat.service.impl;
 
+import com.CasualtyCat.repository.PaymentModeRepository;
+
 public class PaymentServiceImpl {
+    private PaymentModeRepository paymentModeRepository;
 }
